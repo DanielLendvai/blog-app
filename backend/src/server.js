3c15376ae82dc102cd880e0fbd091223d84c57df
Daniel Lendvai <lendvai.daniel92@gmail.com>
2023-01-18 12:19:38 +0100

    comment feature endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,24 @@ let articlesInfo = [
     {
         name: "learn-react",
         upvotes: 0,
+        comments: [],
     },
     {
         name: "learn-node",
         upvotes: 0,
+        comments: [],
     },
     {
         name: "mongodb",
         upvotes: 0,
+        comments: [],
     },
 ];
 
 const app = express();
 app.use(express.json());
 
+//upvote
 app.put("/api/articles/:name/upvote", (req, res) => {
     const { name } = req.params;
     const article = articlesInfo.find((a) => a.name === name);
@@ -29,7 +33,20 @@ app.put("/api/articles/:name/upvote", (req, res) => {
     }
 });
 
+//comment
+app.post("/api/articles/:name/comments", (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
 
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send("that article doesn't exist");
+    }
+});
 
 app.listen(8000, () => {
     console.log("Server is listening on port 8000");
